51c51f5979f2414eaf50fc092f54e8637d9389d0
nischal <nischal123@gmail.com>
2015-07-19 10:10:44 +0545

    git

--- hms/grails-app/domain/hrm/LeaveSetting.groovy
+++ hms/grails-app/domain/hrm/LeaveSetting.groovy
@@ -21,11 +21,43 @@ class LeaveSetting {
         reason(blank:false,nullable: false)
         approveBy(blank:true,nullable: true)
         fromDate(validator:{ val, obj ->
-            def leaveSetting=LeaveSetting.findByEmployee(obj.employee,[sort:'toDate',order:'desc'])
-            if (val> obj.toDate) ['validation.customFromDateScope','FromDate cannot be greater than ToDate']
-            else if(leaveSetting && val<=leaveSetting?.toDate){
-                ['validation.customFromDateTaken', obj.employee.getFullName()+' has already taken leave in selected date']
+                //create and update for sameinstance
+            if (val> obj.toDate) return ['validation.customFromDateScope','FromDate cannot be greater than ToDate']
+            else{
+                //update
+                if(obj?.id){
+                    def leaveSetting=LeaveSetting.findAllByEmployee(obj.employee,[sort:'id',order:'asc'])
+                    def currInd=leaveSetting.indexOf(obj);
+                    def size=leaveSetting.size()
+                    def prev=currInd-1;
+                    def next=currInd+1;
+
+                    if(prev>=0){
+                        if(obj.fromDate <= leaveSetting.get(prev).toDate  ){
+                          return  ['validation.customFromDateTaken', obj.employee.getFullName()+' has already taken leave in selected date. From Date must be greater than '+leaveSetting.get(prev).toDate]
+                        }
+                    }
+
+                    if(size>next){
+                        if(obj.toDate >= leaveSetting.get(next).fromDate){
+                         return   ['validation.customFromDateTaken', obj.employee.getFullName()+' has already taken leave in selected date. To Date must be lesser than '+leaveSetting.get(next).fromDate]
+                        }
+                    }
+
+
+                }else{
+                    //create
+                    def leaveSetting=LeaveSetting.findByEmployee(obj.employee,[sort:'id',order:'desc'])
+
+                    if(leaveSetting && val<=leaveSetting?.toDate){
+                       return ['validation.customFromDateTaken', obj.employee.getFullName()+' has already taken leave in selected date']
+                    }
+                }
+
             }
+
+
+
          })
 
 
